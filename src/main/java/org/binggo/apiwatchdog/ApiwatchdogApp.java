ddf86284e359a067ce0e4f63ebf238f293ee99d8
binggo <binggozhan@163.com>
2016-10-18 11:41:23 +0800

    create the project 'apiwatchdog'

---
+++ src/main/java/org/binggo/apiwatchdog/ApiwatchdogApp.java
@@ -0,0 +1,19 @@
+package org.binggo.apiwatchdog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwatchdogApp {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ApiwatchdogApp.class);
+	
+    public static void main( String[] args ) {
+    	SpringApplication app = new SpringApplication(ApiwatchdogApp.class);
+		app.run(args);
+		
+		logger.info("start apiwatchdog successfully.");
+    }
+}
---
+++ src/test/java/org/binggo/apiwatchdog/ApiwatchdogAppTest.java
@@ -0,0 +1,36 @@
+package org.binggo.apiwatchdog;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApiwatchdogAppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ApiwatchdogAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ApiwatchdogAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
