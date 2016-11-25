c26fde1bd46de2038f2daa4d4811100556fbf2c1
ybzhan <ybzhan@ibenben.com>
2016-11-25 10:05:21 +0800

    update apiwatchdog-monitor.sh

--- scripts/apiwatchdog-monitor.sh
+++ scripts/apiwatchdog-monitor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # ---------------------------------------------------------------------------
-# Description: supervisor apiwatchdog's running status.
-#   add to crontab: Crontab: */2 * * * *  /your/path/apiwatchdog-supervisor.sh
+# Description: monitor apiwatchdog's running status.
+#   add to crontab: Crontab: */1 * * * *  /your/path/apiwatchdog-monitor.sh
 # ---------------------------------------------------------------------------
 
 # configuration of log file
-SUPERVISOR_LOG_FILE=/data0/logs/apiwatchdog/supervisor.log
+MONITOR_LOG_HOME=/data0/apiwatchdog/monitor
+[ -d $MONITOR_LOG_HOME ] || mkdir -p $MONITOR_LOG_HOME
+MONITOR_LOG_FILE=$MONITOR_LOG_HOME/monitor.log
 APIWATCHDOG_LOG_FILE=/dev/null
 
 DIR=$(dirname `readlink -m $0`)
@@ -18,11 +20,11 @@ function log() {
     echo "[$date_str] [$log_level] $log_msg" >> $SUPERVISOR_LOG_FILE
 }
 
-EXIST=$(ps aux | grep "apiwatchdog.properties" | grep -v grep | wc -l)
+EXIST=$(ps aux | grep "apiwatchdog.jar" | grep -v grep | wc -l)
 if [ $EXIST -gt 0 ]; then
 	log INFO "apiwatchdog is running"
 else
     log ERROR "apiwatchdog has stopped"
-    nohup ./scripts/apiwatchdog-start.sh > $APIWATCHDOG_LOG_FILE 2>&1 &
-    log INFO "apiwatchdog-supervisor has restarted apiwatchdog"
+    nohup ./scripts/apiwatchdog-start.sh production 2>&1 $APIWATCHDOG_LOG_FILE &
+    log INFO "apiwatchdog-monitor.sh has restarted apiwatchdog"
 fi
